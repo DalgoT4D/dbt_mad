ac539da8015cc82a0cbacc194de10921c7a2e9e8
Siddhant Singh <ssiddhant3030@gmail.com>
2025-04-18 18:09:45 +0530

    added the intermediate table

---
+++ models/intermediate/user_data_int.sql
@@ -0,0 +1,40 @@
+{{ config(
+  materialized='table'
+) }}
+
+
+with source as (
+  select * from {{ source('source_mad', 'user_data_int') }}
+),
+
+renamed as (
+  select
+    "City"::text as city,
+    "Email"::text as email,
+    "State"::text as state,
+    "Center"::text as center,
+    "UserId"::numeric as user_id,
+    "AddedBy"::text as added_by,
+    "Contact"::text as contact,
+    "UserRole"::text as user_role,
+    "UserLogin"::text as user_login,
+    "UserDisplayName"::text as user_display_name,
+
+    -- Casting only if ISO 8601 or standard format
+    case 
+      when "UserCreatedDateTime" ~ '^\d{4}-\d{2}-\d{2}' then "UserCreatedDateTime"::timestamp
+      else null
+    end as user_created_datetime,
+
+    case 
+      when "UserUpdatedDateTime" ~ '^\d{4}-\d{2}-\d{2}' then "UserUpdatedDateTime"::timestamp
+      else null
+    end as user_updated_datetime,
+
+    "_airbyte_raw_id"::text as _airbyte_raw_id,
+    "_airbyte_extracted_at"::timestamptz as _airbyte_extracted_at,
+    "_airbyte_meta"
+  from source
+)
+
+select * from renamed
