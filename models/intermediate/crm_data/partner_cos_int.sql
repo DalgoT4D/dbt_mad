175e0f433f0ca164a79e3272d034347d6ae398fd
Siddhant Singh <ssiddhant3030@gmail.com>
2025-07-31 19:59:42 +0530

    till here its working

--- models/intermediate/crm_data/partner_cos_int.sql
+++ models/intermediate/crm_data/partner_cos_int.sql
@@ -1,5 +1,27 @@
 {{ config(materialized='table') }}
 
+WITH source_data AS (
+  SELECT *
+  FROM {{ source('crm_data', 'partner_cos') }}
+),
+
+deduplicated AS (
+  {{ dbt_utils.deduplicate(
+      relation='source_data',
+      partition_by='id',
+      order_by='"updatedAt" desc',
+     )
+  }}
+)
+
+-- Partner to Community Organizer assignments
 SELECT
-   * 
-FROM {{ source('crm_data', 'partner_cos') }} 
+   id::text,
+   partner_id::text,
+   co_id::text,
+   "createdAt" AS created_at,
+   "updatedAt" AS updated_at,
+   _airbyte_raw_id,
+   _airbyte_extracted_at,
+   _airbyte_meta
+FROM deduplicated
